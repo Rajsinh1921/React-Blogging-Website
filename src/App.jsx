18ed57ba85405606ffd6d97471eeff19dd58da66
Rajsinh Chauhan <rajsinh.chauhan15@gmail.com>
2023-12-12 12:22:23 -0500

    Test

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
 import { Outlet } from "react-router-dom";
 function App() {
+  console.log(`test`);
   const [loading, setLoading] = useState(true);
   const dispatch = useDispatch();
 
